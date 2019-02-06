7be3a3b35b25eb577ee8632cdcb2ee6560243693
JaeggerMeisster <manuel.jager@hotmail.nl>
2019-02-06 12:01:33 +0100

    Reken OPs

---
+++ Dragon Slayer 2D/Assets/Scripts/Reken-OPs/RekenOpsTranslator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RekenOps : MonoBehaviour
+{
+
+
+    int GetDisplayNums(int field)
+    {
+        return field;
+    }
+
+    int ThrowSelectedField(int field)
+    {
+        return field;
+    }
+}
