3b35bec81ce7dc3393977df98f28f600e511b66e
Myrina <jinkisaragi@hotmail.com>
2019-02-11 11:32:12 +0100

    destructible objects TEST

--- Dragon Slayer 2D/Assets/DestructableObjects.cs
+++ Dragon Slayer 2D/Assets/DestructableObjects.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class DestructableObjects : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject throwableAxe;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.name.Equals("Player"))
+        {
+            bool isAttacking = collision.gameObject.GetComponent<PlayerControl>().isAttacking;
+
+            if (isAttacking)
+            {
+                Instantiate(throwableAxe,gameObject.transform.position, gameObject.transform.rotation);
+                Destroy(gameObject);
+
+            }
+        }
     }
 }
